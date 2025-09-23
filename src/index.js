8a464904681079024acb84a1f7032495c98b7e07
JuanCM <juancampuzano2356@gmail.com>
2025-09-23 08:44:13 -0500

    feat(index) Add routes api 'auth' 'categories' 'users'

--- src/index.js
+++ src/index.js
@@ -13,6 +13,19 @@ app.use(express.json());
 // Rutas
 app.use("/api/posts", postRoutes);
 
+app.use("/api/users", (req, res) => {
+    res.send("User routes will be here");
+});
+
+app.use("/api/categories", (req, res) => {
+    res.send("Category routes will be here");
+});
+
+app.use("/api/auth", (req, res) => {
+    res.send("Auth routes will be here");
+});
+
+
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
