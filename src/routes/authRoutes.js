ba913ebf4b95e0bb5d47ca276287b3863aa9df0d
JuanCM <juancampuzano2356@gmail.com>
2025-09-24 10:36:24 -0500

    feat(authRoutes) Add authRoutes, '/register/, '/login', '/token'

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import * as authController from '../controllers/authController.js';
+
+const router = Router();
+
+router.post('/register', authController.registerUser);
+router.post('/login', authController.loginUser);
+router.post('/token', authController.getNewToken);
+
+export default router;
