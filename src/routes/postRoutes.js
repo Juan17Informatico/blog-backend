7f2a0d4f64763648e64756c01a99ff62c0be0a6e
JuanCM <juancampuzano2356@gmail.com>
2025-09-27 11:11:40 -0500

    feat(postRoutes): Add middleware for protected endpoints

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import * as postController from "../controllers/postController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
+// publics
 router.get("/", postController.getPosts);
 router.get("/:id", postController.getPost);
-router.post("/", postController.create);
-router.put("/:id", postController.update);
-router.delete("/:id", postController.remove);
+
+// protected
+router.post("/", authMiddleware,  postController.create);
+router.put("/:id", authMiddleware, postController.update);
+router.delete("/:id", authMiddleware, postController.remove);
 
 export default router;
